c4bc8f978bb5dae733dc87e73f7a375c268a542a
Andrew-ITP-Eminov <eminov.itpremium@gmail.com>
2020-03-02 17:04:54 +0300

    [WIP] fix mobile  menu

--- src/components/Header/MobileNavbar.jsx
+++ src/components/Header/MobileNavbar.jsx
@@ -7,8 +7,14 @@ import "./hamburgers.scss";
 function MobileNavbar({list, listClass, tabClass}) {
 
     const [anchorEl, setAnchorEl] = React.useState(null);
+    const [open, setOpen] = React.useState(null);
 
     const handleClick = event => {
+        if (open){
+            setOpen(null);
+            setAnchorEl(null);
+        }
+        setOpen(true);
         setAnchorEl(event.currentTarget);
     };
 
@@ -18,34 +24,25 @@ function MobileNavbar({list, listClass, tabClass}) {
 
     return (
         <div className={listClass}>
-            <nav role="navigation"  onClick={handleClick}>
+            <nav role="navigation" onClick={handleClick}>
                 <div id="menuToggle">
                     <input type="checkbox" />
                     <span></span>
                     <span></span>
                     <span></span>
-
-                    <Menu
-                        id="simple-menu"
-                        anchorEl={anchorEl}
-                        // keepMounted
-                        open={Boolean(anchorEl)}
-                        // onClose={handleClose}
-                    >
-                        {list.map((value, index)=>
-                            <MenuItem onClick={handleClose} className={tabClass} key={index}>{value}</MenuItem>
-                        )}
-                    </Menu>
                 </div>
-
-
             </nav>
-            {/*<ul className={listClass}>*/}
-            {/*    {list.map((value, index)=>*/}
-            {/*        <li onClick={handleClose} className={tabClass} key={index}>{value}</li>)*/}
-            {/*    }*/}
-            {/*</ul>*/}
-
+            {open &&  <Menu
+                id="simple-menu"
+                anchorEl={anchorEl}
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={handleClick}
+            >
+                {list.map((value, index)=>
+                    <MenuItem className={tabClass} key={index}>{value}</MenuItem>
+                )}
+            </Menu>}
         </div>
     );
 }
