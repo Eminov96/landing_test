07185db937f2467ac0b3d9efb49ed3d20b7a3db7
Andrew-ITP-Eminov <eminov.itpremium@gmail.com>
2020-03-11 16:03:17 +0300

    fix by pixel perfect

--- src/components/CommonComponents/Carousel.jsx
+++ src/components/CommonComponents/Carousel.jsx
@@ -1,24 +1,45 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
 import {connect} from "react-redux";
 import Slider from 'infinite-react-carousel';
 import {setSlide} from "../../actions/actionsMainPage";
+
+import FullScreen, { fullScreenSupported } from 'react-request-fullscreen';
+
 import './common.scss'
 
 function Carousel({sliderClassName, managingSliderClassName, setSlide, carouselImages, currentSlide}) {
+
+    const { isFullScreen } = useState(false);
+    let elFullScreenRef = useRef(null);
+    let elRef = useRef(null);
 
     const changeCurrentSlide = (index) => {
         setSlide(index);
     };
 
+    const requestOrExitFullScreenByElement = () => {
+        elFullScreenRef.fullScreen(elRef);
+    };
+
     return (
-        <div className={sliderClassName} >
-            <Slider duration={150} className={''} afterChange={changeCurrentSlide} >
+        <div className={sliderClassName} ref={ref => { elRef = ref }}>
+            <Slider duration={150} className={''} afterChange={changeCurrentSlide}>
                 {carouselImages && carouselImages.map((image, index)=>
                     <img className={'carouselImage'} key={index} src={image} alt=""/>
                 )}
             </Slider>
             <div className={managingSliderClassName}>
                 <span>{currentSlide+1}/{carouselImages.length}</span>
+
+                {/*<FullScreen ref={ref => {elFullScreenRef = ref }}>*/}
+                {/*    <div*/}
+                {/*        className='el-rq'*/}
+                {/*        onClick={requestOrExitFullScreenByElement}*/}
+                {/*    >*/}
+                {/*        {!isFullScreen ? 'Request FullScreen by Element' : 'Exit FullScreen by Element'}*/}
+                {/*    </div>*/}
+                {/*</FullScreen>*/}
+
             </div>
         </div>
     );
